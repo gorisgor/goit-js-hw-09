0b9b5f5059d9e3ac6a37ce8208b2d7296d73d9d9
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-04-23 15:05:57 +0300

    Update

---
+++ src/js/2-form.js
@@ -0,0 +1,20 @@
+const form = document.querySelector(".feedback-form");
+const userEmail = form.elements.email
+const textarea = form.elements.message;
+const localStorageEmail = "userEmail";
+const localStorageMessage = "userMessage"
+userEmail.value = localStorage.getItem(localStorageEmail) ?? "";
+textarea.value = localStorage.getItem(localStorageMessage) ?? "";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageEmail, evt.target.value);
+  localStorage.setItem(localStorageMessage, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+	console.log(evt.target.elements.message.value);
+  localStorage.removeItem(localStorageEmail);
+  localStorage.removeItem(localStorageMessage);
+  form.reset();
+});
